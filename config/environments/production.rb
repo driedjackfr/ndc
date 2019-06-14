2cc5186b60b4518457cd205fa00928fd23e5ced3
purin <purin@MacBook-Pro-cua-purin.local>
2019-06-14 23:47:33 +0700

    use gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
     :domain => 'ndc-blog.herokuapp.com',
-    :address => 'smtp.sendgrid.net',
+    :address => 'smtp.gmail.com',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
