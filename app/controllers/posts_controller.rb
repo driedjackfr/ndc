999d2920254faafcf065af5abc80ce969595b789
cuong-nguyen <42411756+driedjack@users.noreply.github.com>
2019-06-15 12:38:01 +0700

    Improve style and performance (#2)
    * Donot query body in post index, quote has author
    
    * destroy and css form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@
 
 class PostsController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc)
+    @posts = Post.select(:id, :title, :excerpt, :created_at).order(created_at: :desc)
   end
 
   def show
