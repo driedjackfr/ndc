f7986421682916adba1c1cddeef1c5bdcc54c1d5
driedjack <driedjack@gmail.com>
2019-06-20 22:55:49 +0700

    text

--- app/javascript/components/caro_comp.vue
+++ app/javascript/components/caro_comp.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="caro-comp">
     <h1>Chơi caro nào!</h1>
-    <button>Chơi với bạn</button>
-    <button>{{ message }}</button>
+    <button class="btn">Chơi với máy</button>
+    <button class="btn">Chơi với bạn</button>
   </div>
 </template>
 
@@ -16,8 +16,11 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 #caro-comp {
   text-align: center;
+  h1 {
+    margin-bottom: 10px;
+  }
 }
 </style>
