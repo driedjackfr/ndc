93d95ef8b891886241f986082a002b602d9f3dbc
driedjack@gmail.com <driedjack@gmail.com>
2019-07-10 16:55:46 +0700

    font

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << "38f7cf80.ngrok.io"
 end
